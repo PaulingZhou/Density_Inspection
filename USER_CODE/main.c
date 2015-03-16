179d33242154f2ef5a2c5b73e258cc28f58ce319
PaulingZhou <1152513@tongji.edu.cn>
2015-03-16 19:11:02 +0800

    该提交使lpc1114的PIO2.7为DATA总线，PIO2.8为CLK总线 可模拟出开始发送命令该提交使lpc1114的PIO2.7为DATA总线，PIO2.8为CLK总线

--- USER_CODE/main.c
+++ USER_CODE/main.c
@@ -1,95 +1,178 @@
-/****************************************Copyright (c)****************************************************
-**                            Guangzhou ZLGMCU Development Co., LTD
-**
-**                                 http://www.zlgmcu.com
-**
-**--------------File Info---------------------------------------------------------------------------------
-** File name:           main.c
-** Last modified Date:  2010-02-04
-** Last Version:        V1.0
-** Descriptions:        The main() function example template
-**
-**--------------------------------------------------------------------------------------------------------
-** Created by:          Li Baihua
-** Created date:        2010-09-10
-** Version:             V1.00
-** Descriptions:        整理模板，添加用户应用程序
-**
-**--------------------------------------------------------------------------------------------------------
-** Modified by:         
-** Modified date:       
-** Version:             
-** Descriptions:        
-**
-**--------------------------------------------------------------------------------------------------------
-** Modified by:        
-** Modified date:      
-** Version:            
-** Descriptions:       
-**
-** Rechecked by:
-*********************************************************************************************************/
-#include "LPC11xx.h"                                                    /* LPC11xx外设寄存器            */
+    /****************************************Copyright (c)****************************************************
+    **                                          
+    **
+    **
+    **--------------File Info---------------------------------------------------------------------------------
+    ** File name:           main.c
+    ** Last modified Date:  2015-3-15
+    ** Last Version:        V1.0
+    ** Descriptions:        The main() function 
+    **
+    **--------------------------------------------------------------------------------------------------------
+    ** Created by:          Zhou Sihang
+    ** Created date:        2015-3-15
+    ** Version:             V1.00
+    ** Descriptions:        气体浓度计
+    **
+    **--------------------------------------------------------------------------------------------------------
+    ** Modified by:         
+    ** Modified date:       
+    ** Version:             
+    ** Descriptions:        
+    **
+    **--------------------------------------------------------------------------------------------------------
+    ** Modified by:        
+    ** Modified date:      
+    ** Version:            
+    ** Descriptions:       
+    **
+    ** Rechecked by:
+    *********************************************************************************************************/
+    #include "LPC11xx.h"                                                    /* LPC11xx外设寄存器            */
 
-/*********************************************************************************************************
-  宏定义
-*********************************************************************************************************/
-#define BEEP            (1ul << 7)
+    /*********************************************************************************************************
+      宏定义
+    *********************************************************************************************************/
+    #define DATAHIGH            (1ul << 7)           //DATA总线为PIO2_7,DATABHIGH表示数据总线高电平
+    #define SCKHIGH             (1ul << 8)           //SCK为总线PIO2_8,SCKHIGH表示时钟线高电平
+    #define PIO2_7_FUNC         0x00                 //把PIO2_7设置为GPIO功能
+    #define PIO2_8_FUNC         0x00                 //把PIO2_8设置为GPIO功能
+    #define PIO2_MODE           0x02                 //把PIO2_7,PIO2_8设置为内部上拉使能
 
-/*********************************************************************************************************
-** Function name:       myDelay
-** Descriptions:        软件延时
-** input parameters:    无
-** output parameters:   无
-** Returned value:      无
-*********************************************************************************************************/
-void myDelay (uint32_t ulTime)
-{
-    uint32_t i;
+    #define noACK               0
+    #define ACK                 1
+                                                    //adr command r/w
+    #define STATUS_REG_W 0x06                       //000  0011    0
+    #define STATUS_REG_R 0x07                       //000  0011    1
+    #define MEASURE_TEMP 0x03                       //000  0001    1
+    #define MEASURE_HUMI 0x05                       //000  0010    1
+    #define RESET 0x1e                              //000  1111    0
 
-    i = 0;
-    while (ulTime--) {
-        for (i = 0; i < 5000; i++);
+
+    /*********************************************************************************************************
+    ** Function name:       s_write_byte
+    ** Descriptions:        从Sensibus上写一个字节并检查是否有回应
+    ** input parameters:    无
+    ** output parameters:   无
+    ** Returned value:      无
+    *********************************************************************************************************/
+    // char s_write_byte(uint8_t value)
+    // {
+    //     uint8_t i,error = 0;
+    //     for(i=0x80;i>0;i/=2){                   //shift bit for masking,0x80=1000 0000
+    //         if (i & value) LPC_GPIO0->DATA |= DATAHIGH;              //masking value with i,write to SENSI-BUS
+    //         else LPC_GPIO0->DATA &= ~DATAHIGH;;
+    //         LPC_GPIO0->DATA |= SCKHIGH;
+    //         for(i=0;i<3;i++);
+    //         LPC_GPIO0->DATA &= ~SCKHIGH;
+    //     }
+    //     LPC_GPIO0->DATA |= DATAHIGH;
+    //     LPC_GPIO0->DATA |= SCKHIGH;
+    //     error=DATAHIGH;
+    //     LPC_GPIO0->DATA &= ~SCKHIGH;
+    //     return error;
+    // }
+    
+        /*********************************************************************************************************
+    ** Function name:       myDelay
+    ** Descriptions:        软件延时
+    ** input parameters:    无
+    ** output parameters:   无
+    ** Returned value:      无
+    *********************************************************************************************************/
+    void myDelay (uint32_t ulTime)
+    {
+        uint32_t i;
+
+        i = 0;
+        while (ulTime--) {
+            for (i = 0; i < 5000; i++);
+        }
     }
-}
 
-/*********************************************************************************************************
-** Function name:       GPIOInit
-** Descriptions:        GPIO初始化
-** input parameters:    无
-** output parameters:   无
-** Returned value:      无
-*********************************************************************************************************/
-void GPIOInit( void )
-{
-    LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 6);                              /* 初始化GPIO AHB时钟           */
-    LPC_IOCON->PIO2_7 &= ~0x07;                                         /* 将P2.7初始化为GPIO功能       */
-	LPC_GPIO2->DIR    |= BEEP;                                          /* 将P2.7方向设置为输出         */
-	LPC_GPIO2->DATA   |= BEEP;                                          /* 将P2.7初始化输出高电平       */
-}
-
-/*********************************************************************************************************
-** Function name:       main
-** Descriptions:        BEEP测试程序
-**                      跳线连接：短接P2.7与蜂鸣器BEEP控制引脚；
-**                      现    象：下载代码，运行程序，则BEEP交替鸣叫
-** input parameters:    无
-** output parameters:   无
-** Returned value:      无
-*********************************************************************************************************/
-int main (void)
-{
-    SystemInit();                                                       /* 系统初始化，切勿删除         */
-
-    GPIOInit();
-    while (1) {
-        LPC_GPIO2->DATA |= BEEP;
-        myDelay(400); 
-        LPC_GPIO2->DATA &= (~BEEP);
-        myDelay(400);
+    /*********************************************************************************************************
+    ** Function name:       s_transstart
+    ** Descriptions:        产生传输开始指令
+    ** input parameters:    无
+    ** output parameters:         __      _____
+                            DATA:   |____|
+                                   __    __
+                            SCK: _|  |__|  |___
+    ** Returned value:      无
+    *********************************************************************************************************/
+    void s_transstart(void){
+            LPC_GPIO2->DIR  |= (DATAHIGH|SCKHIGH);                              /* 将p2.7,p2.8方向寄存器置1(配置为输出) */
+            LPC_GPIO2->DATA |= DATAHIGH;        //DATA=1
+            LPC_GPIO2->DATA &= ~SCKHIGH;        //SCK=0
+            myDelay(10);
+            LPC_GPIO2->DATA |= SCKHIGH;         //SCK=1
+            myDelay(10);
+            LPC_GPIO2->DATA &= ~DATAHIGH;       //DATA=0
+            myDelay(10);
+            LPC_GPIO2->DATA &= ~SCKHIGH;        //SCK=0
+            myDelay(30);
+            LPC_GPIO2->DATA |= SCKHIGH;         //SCK=1
+            myDelay(10);
+            LPC_GPIO2->DATA |= DATAHIGH;        //DATA=1
+            myDelay(10);
+            LPC_GPIO2->DATA &= ~SCKHIGH;        //SCK=0
+            myDelay(10);
+            LPC_GPIO2->DIR  &= ~(DATAHIGH|SCKHIGH);                              /* 将p2.7,p2.8方向寄存器置0(配置为输入) */
     }
-}
 
-/*********************************************************************************************************
-  End Of File
-*********************************************************************************************************/
+
+
+    /*********************************************************************************************************
+    ** Function name:       GPIOInit
+    ** Descriptions:        GPIO初始化
+    ** input parameters:    无
+    ** output parameters:   无
+    ** Returned value:      无
+    *********************************************************************************************************/
+    void GPIOInit( void )
+    {
+        LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 6);                              /* 初始化GPIO AHB时钟       */
+        LPC_IOCON->PIO2_7 = PIO2_7_FUNC|(PIO2_MODE<<3);                     /* 将P2.7定义为GPIO功能，上拉电阻使能      */
+        LPC_IOCON->PIO2_8 &= PIO2_8_FUNC|(PIO2_MODE<<3);                    /* 将p2.8定义为GPIO功能，上拉电阻使能       */
+        LPC_GPIO2->DIR  |= (DATAHIGH|SCKHIGH);                              /* 将p2.7,p2.8方向寄存器置1(配置为输出) */
+        LPC_GPIO2->DATA |= (DATAHIGH|SCKHIGH);                              /* 将p2.7,p2.8配置为初始化输出高电平 */          
+    }
+
+    /*********************************************************************************************************
+    ** Function name:       main
+    ** Descriptions:        无
+    ** input parameters:    无
+    ** output parameters:   无
+    ** Returned value:      无
+    *********************************************************************************************************/
+    int main (void)
+    {
+        SystemInit();                                                       /* 系统初始化，切勿删除         */
+
+        GPIOInit();
+        while (1) {
+//             LPC_GPIO2->DIR  |= (DATAHIGH|SCKHIGH);                              /* 将p2.7,p2.8方向寄存器置1(配置为输出) */
+//             LPC_GPIO2->DATA |= DATAHIGH;        //DATA=1
+//             LPC_GPIO2->DATA &= ~SCKHIGH;        //SCK=0
+//             myDelay(10);
+//             LPC_GPIO2->DATA |= SCKHIGH;         //SCK=1
+//             myDelay(10);
+//             LPC_GPIO2->DATA &= ~DATAHIGH;       //DATA=0
+//             myDelay(10);
+//             LPC_GPIO2->DATA &= ~SCKHIGH;        //SCK=0
+//             myDelay(30);
+//             LPC_GPIO2->DATA |= SCKHIGH;         //SCK=1
+//             myDelay(10);
+//             LPC_GPIO2->DATA |= DATAHIGH;        //DATA=1
+//             myDelay(10);
+//             LPC_GPIO2->DATA &= ~SCKHIGH;        //SCK=0
+//             myDelay(10);
+//             LPC_GPIO2->DIR  &= ~(DATAHIGH|SCKHIGH);                              /* 将p2.7,p2.8方向寄存器置0(配置为输入) */
+            s_transstart();
+            myDelay(1000);
+        }
+    }
+
+    /*********************************************************************************************************
+      End Of File
+    *********************************************************************************************************/
